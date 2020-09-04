e6cc4211762e72b25a47165cc2ef2f3c0fe418b8
drandek <daniel.randek@gmail.com>
2020-09-04 09:45:17 +0200

    Person constructor és getFullName metódus.

--- src/main/java/dransoft/Person.java
+++ src/main/java/dransoft/Person.java
@@ -1,10 +1,16 @@
 package dransoft;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor(staticName = "of")
 public class Person {
 
     @Getter private String firstName;
     @Getter private String lastName;
     @Getter private int yob;
+
+    public String getFullName(){
+        return lastName + " " + firstName;
+    }
 }
