5c02682b16336f7b57b83560741ac990ac093144
drandek <daniel.randek@gmail.com>
2020-09-04 11:33:12 +0200

    Kicseréltem a bufferedreadert egy Scannerre.

--- src/main/java/dransoft/App.java
+++ src/main/java/dransoft/App.java
@@ -1,10 +1,9 @@
 package dransoft;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Maven projekt main osztály
@@ -20,25 +19,22 @@ public class App
      * getFullName metódusát az összesre és kiírja az így visszakapott
      * sztringet.
      */
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws FileNotFoundException {
         String personsCSV = "/Users/dradnek/maven/first-mavenapp/src/main/resources/persons.txt";
-        String line;
-        String splitBy = ",";
+        File personFile = new File(personsCSV);
         List<Person> people = new ArrayList<>();
+        Scanner scanner = new Scanner(personFile);
+        String splitBy = ",";
+        String line;
 
-        try (BufferedReader br = new BufferedReader(new FileReader(personsCSV))) {
+        while (scanner.hasNextLine()){
+            line = scanner.nextLine();
+            String[] personData = line.split(splitBy);
+            Person person = new Person(personData[0], personData[1], Integer.parseInt(personData[2]));
+            people.add(person);
 
-            while ((line = br.readLine()) != null) {
-
-                String[] personData = line.split(splitBy);
-                Person person = new Person(personData[0], personData[1], Integer.parseInt(personData[2]));
-                people.add(person);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+        scanner.close();
 
         for (Person p : people) {
             System.out.println(p.getFullName());
