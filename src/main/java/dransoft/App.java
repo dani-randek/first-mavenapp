3b330591fa3e19c162f33c281d8aafbddd11204d
drandek <daniel.randek@gmail.com>
2020-09-04 11:38:34 +0200

    a

--- src/main/java/dransoft/App.java
+++ src/main/java/dransoft/App.java
@@ -1,40 +1,41 @@
 package dransoft;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 /**
  * Maven projekt main osztály
  *
  */
-public class App 
+public class App
 {
 
     /**
-     * A program main metódusa
-     * Beolvassa a persons.txt-ből a tárolt adatokat, majd eltárolha
-     * óket egy Person arraylistben, végül pedig meghívja a Person
-     * getFullName metódusát az összesre és kiírja az így visszakapott
-     * sztringet.
+     * Main metódus
+     * Beolvassa a persons.txt-ből a tárolt adatokat
      */
-    public static void main( String[] args ) throws FileNotFoundException {
+    public static void main( String[] args )
+    {
         String personsCSV = "/Users/dradnek/maven/first-mavenapp/src/main/resources/persons.txt";
-        File personFile = new File(personsCSV);
-        List<Person> people = new ArrayList<>();
-        Scanner scanner = new Scanner(personFile);
-        String splitBy = ",";
         String line;
+        String splitBy = ",";
+        List<Person> people = new ArrayList<>();
 
-        while (scanner.hasNextLine()){
-            line = scanner.nextLine();
-            String[] personData = line.split(splitBy);
-            Person person = new Person(personData[0], personData[1], Integer.parseInt(personData[2]));
-            people.add(person);
+        try (BufferedReader br = new BufferedReader(new FileReader(personsCSV))) {
 
+            while ((line = br.readLine()) != null) {
+
+                String[] personData = line.split(splitBy);
+                Person person = new Person(personData[0], personData[1], Integer.parseInt(personData[2]));
+                people.add(person);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        scanner.close();
 
         for (Person p : people) {
             System.out.println(p.getFullName());
